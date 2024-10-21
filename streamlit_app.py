c6e3a3c94840425f75bc07aa8f322e2ae538de35
Ricardoibanezf <156374392+Ricardoibanezf@users.noreply.github.com>
2024-10-21 11:08:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ from transformers import pipeline
 BUID= 57631318
 
 generator = pipeline('text-generation', model='gpt2')
+tok_number = st.number_input("Number of tokens for this answer:", min_value=1, value=100, max_value=1000)
+prompt = st.text_input("What is your prompt today?")
+
 
 ### Generate the answer to the question "Damascus is a"
 generator(prompt, max_length=tok_number, num_return_sequences=10, truncation=True)
@@ -22,7 +25,6 @@ set_seed(BUID)
 st.title("Homework 6 - GPT-2 Chatbot")
 
 
-tok_number = st.number_input("Number of tokens for this answer:", min_value=1, value=100, max_value=1000)
-prompt = st.text_input("What is your prompt today?")
+
 
 
