66bb67f6a8477ebaa8e26d565174f222bcc40034
Ricardoibanezf <156374392+Ricardoibanezf@users.noreply.github.com>
2024-10-21 10:59:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
 
 BUID= 57631318
 
-prompt = st.text_input("What is your prompt today?")
+
 
 # Set a seed for the built-in Python random module
 random.seed(BUID)
@@ -22,15 +22,10 @@ np.random.seed(BUID)
 set_seed(BUID)
 
 
-### Create a GPT2 generator pipeline
-generator = pipeline(task='text-generation', model='gpt2')
+st.title("GPT-2 Chatbot")
+st.write("This is a chatbot that uses GPT-2 model to generate answers.")
 
-### Generate the answer to the question "Damascus is a"
-generator(prompt, max_length=20, num_return_sequences=10, truncation=True)
-
-### Print all 10 completions:
-for i in range(10):
-  st.write(response.choices[i].message.content)
+token_length = st.number_input("Number of tokens for this repsonse:", min_value=1, value=50)
+prompt = st.text_input("What is your prompt today?")
 
 
-# Load model directly
