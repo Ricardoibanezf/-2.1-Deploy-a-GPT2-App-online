d51773d020c3f16cd432e9446d3cd2f834b65666
Ricardoibanezf <156374392+Ricardoibanezf@users.noreply.github.com>
2024-10-21 11:52:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import os
 from transformers import pipeline
 
 BUID= 57631318
+st.title("Homework 6 - GPT-2 Chatbot")
 
 generator = pipeline('text-generation', model='gpt2')
 tok_number = st.number_input("Number of tokens for this answer:", min_value=1, value=100, max_value=1000)
@@ -13,14 +14,14 @@ prompt = st.text_input("What is your sentence?")
 A1 = generator(prompt, max_length=tok_number, temperature = 1.0, num_return_sequences=1, truncation=True)
 A2 = generator(prompt, max_length=tok_number, temperature = 0.3, num_return_sequences=1, truncation=True)
 
-st.title("Homework 6 - GPT-2 Chatbot")
 
-st.write("Response 1 *Creative* :sunglasses:")
+
+st.write("Text 1 (Creative)")
 st.write(
     A1[0]['generated_text']
 )
 
-st.write("Response 2 *Predictable* :disappointed:")
+st.write("Text 2 (Predictable) ")
 st.write(
     A2[0]['generated_text']
 )
