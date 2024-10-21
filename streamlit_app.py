a92ca127568cf1d18c9b00352561102e7990ddee
Ricardoibanezf <156374392+Ricardoibanezf@users.noreply.github.com>
2024-10-21 10:51:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ set_seed(BUID)
 
 
 ### Create a GPT2 generator pipeline
-generator = pipeline('text-generation', model='gpt2')
+generator = pipeline(task='text-generation', model='gpt2')
 
 ### Generate the answer to the question "Damascus is a"
 generator("Damascus is a", max_length=20, num_return_sequences=10, truncation=True)
