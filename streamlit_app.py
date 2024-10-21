e51878421d61a0bc21acb8398f99f50505df49e8
Ricardoibanezf <156374392+Ricardoibanezf@users.noreply.github.com>
2024-10-21 10:44:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,28 @@
 import streamlit as st
 from openai import OpenAI
 import os
+import random
+import numpy as np
+import transformers
+from transformers import set_seed
+from transformers import pipeline
 
-BUID= 1679203
-### Load your API Key
-my_secret_key =st.secrets['MyOpenAIKey']
-os.environ["OPENAI_API_KEY"] = my_secret_key
+BUID= 57631318
 
 
-### Request the answer to the question "Damascus is a"
-client = OpenAI()
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "Complete the following prefix"},
-    {"role": "user", "content": "Damascus is a"}
-  ],
-  seed = BUID,
-  n=10,
-  max_tokens=20
-)
+# Set a seed for the built-in Python random module
+random.seed(BUID)
+# Set a seed for NumPy
+np.random.seed(BUID)
+# Set a seed for HuggingFace
+set_seed(BUID)
+
+
+### Create a GPT2 generator pipeline
+generator = pipeline('text-generation', model='gpt2')
+
+### Generate the answer to the question "Damascus is a"
+generator("Damascus is a", max_length=20, num_return_sequences=10, truncation=True)
 
 ### Print all 10 completions:
 for i in range(10):
