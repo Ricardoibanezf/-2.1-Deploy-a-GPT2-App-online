aa810afaefb7a68f6f014d8f3e4a10bd072df32d
Ricardoibanezf <156374392+Ricardoibanezf@users.noreply.github.com>
2024-10-21 11:14:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,22 @@
 import streamlit as st
 import os
-import random
-import numpy as np
 from transformers import pipeline
 
 BUID= 57631318
 
 generator = pipeline('text-generation', model='gpt2')
 tok_number = st.number_input("Number of tokens for this answer:", min_value=1, value=100, max_value=1000)
-prompt = st.text_input("What is your prompt today?")
+prompt = st.text_input("What is your sentence?")
 
 
 ### Generate the answer to the question "Damascus is a"
-generator(prompt, max_length=tok_number, num_return_sequences=10, truncation=True)
-
-# Set a seed for the built-in Python random module
-random.seed(BUID)
-# Set a seed for NumPy
-np.random.seed(BUID)
-# Set a seed for HuggingFace
-set_seed(BUID)
-
+A1 = generator(prompt, max_length=tok_number, temperature = 1.0, num_return_sequences=1, truncation=True)
+A2 = generator(prompt, max_length=tok_number, temperature = 0.3, num_return_sequences=1, truncation=True)
 
 st.title("Homework 6 - GPT-2 Chatbot")
 
-
+st.write(A1)
+st.write(A2)
 
 
 
